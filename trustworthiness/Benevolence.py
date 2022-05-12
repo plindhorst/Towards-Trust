12d1547b537adc91975eadaf7ec6b3bec84811e6
Alto Delia <a.delia@student.tudelft.nl>
2022-05-12 21:01:09 +0200

    Added communication score methods: # ratio of communication related to baby gender # ratio of communication related to yes/no answers # ratio of communication related to room searches

--- trustworthiness/Benevolence.py
+++ trustworthiness/Benevolence.py
@@ -10,3 +10,51 @@ class Benevolence:
     def compute(self):
         score = 0
         return score
+
+    def _communicated_baby_gender(self):
+        count = 0
+        total = 0
+        for i, action in self._actions:
+            if type(action) is MessageAskGender:
+                total += 1
+
+        for j, action in self._actions:
+            if type(action) is MessageBoy | type(action) is MessageGirl:
+                count += 1
+
+        if count > total:
+            return 1
+
+        return count / total
+
+    def _communicated_yes_no(self):
+        count = 0
+        total = 0
+        for i, action in self._actions:
+            if type(action) is MessageRequestPickup:
+                total += 1
+
+        for j, action in self._actions:
+            if type(action) is MessageYes | type(action) is MessageNo:
+                count += 1
+
+        if count > total:
+            return 1
+
+        return count / total
+
+    def _communicated_room_search(self):
+        count = 0
+        total = 0
+        for i, action in self._actions:
+            if type(action) is EnterRoom:
+                total += 1
+
+        for j, action in self._actions:
+            if type(action) is MessageSearch:
+                count += 1
+
+        if count > total:
+            return 1
+
+        return count / total
