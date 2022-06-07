c45c8a7649c91c567c3fc54ed805b35174ae432d
Christopher <C.A.Obameobiang@student.tudelft.nl>
2022-06-07 19:00:41 +0200

    HelpSeekerAgent.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 
 import requests
 
+from trustworthiness.QuestionnaireGraph import QuestionnaireGraph
 from trustworthiness.Trustworthiness import Trustworthiness
 from world.agents.custom.FriendlyAgentDutch import FriendlyAgentDutch
 from world.agents.custom.TutorialAgent import TutorialAgent
@@ -30,10 +31,17 @@ if __name__ == "__main__":
                          friendly, friendly-dutch, tutorial, tutorial-dutch",
                         type=str)
     parser.add_argument("-tw", action='store_true', help="Measure trustworthiness of action files", default=False)
+    parser.add_argument("-qg", action='store_true', help="Plots abi score of questionnaires", default=False)
+
     args = parser.parse_args()
 
     if args.tw:
         trustworthiness = Trustworthiness()
+        sys.exit(0)
+
+    if args.qg:
+        questionnaireGraph = QuestionnaireGraph()
+        print("got here")
         sys.exit(0)
 
     agent = None
